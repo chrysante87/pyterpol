2699b3c5cc48e3bae9b50b02531c5f411d0b318c
chrysante87 <jana.nemravova@gmail.com>
2015-12-04 12:01:54 +0100

    Fixed - was using slow interpolation environment. Now it is much faster.

--- synthetic/auxiliary.py
+++ synthetic/auxiliary.py
@@ -7,6 +7,7 @@ from scipy.signal import fftconvolve
 
 ZERO_TOLERANCE = 1e-6
 
+
 def interpolate_block(x, block, xnew):
     """
     Interpolates in each column of a 2d matrix.
@@ -17,27 +18,28 @@ def interpolate_block(x, block, xnew):
 	intens.. interpolated
     """
     intens = np.zeros(len(block[0]))
-    n = len(block[:,0])
-    
+    n = len(block[:, 0])
+
     # set up the order of interpolation
     if n > 4:
-	k = 3
+        k = 3
     else:
-	k = n-1
-    #k=3
-    
+        k = n - 1
+    # k=3
+
+    # TODO Can thius be done faster with bisplrep and bisplev
     # do the interpolation
     for i in range(0, len(block[0])):
-	y = block[:,i]
+        y = block[:, i]
 
-	#tck = splrep(x, y, k=k)
-	#intens[i] = splev(xnew, tck, der=0)
-	#f = interp1d(np.array(x), np.array(y), kind='cubic')
-	#intens[i] = f(xnew)
-	intens[i] = spline(x, y, xnew, order=k)
-	
+        tck = splrep(x, y, k=k)
+        intens[i] = splev(xnew, tck, der=0)
+        # f = interp1d(np.array(x), np.array(y), kind='cubic')
+        # intens[i] = f(xnew)
+        # intens[i] = spline(x, y, xnew, order=k)
+
     return intens
-	
+
 
 def interpolate_spec(wave0, intens0, wave1):
     """
@@ -49,8 +51,9 @@ def interpolate_spec(wave0, intens0, wave1):
     """
     tck = splrep(wave0, intens0)
     intens1 = splev(wave1, tck)
-    
+
     return intens1
+
 
 def is_within_interval(v, arr):
     """
@@ -66,6 +69,7 @@ def is_within_interval(v, arr):
     else:
         return True
 
+
 def generate_least_number(l):
     """
     Goes over integer in list and finds the
@@ -77,6 +81,7 @@ def generate_least_number(l):
     while num in l:
         num += 1
     return num
+
 
 def keys_to_lowercase(d):
     """
@@ -92,6 +97,7 @@ def keys_to_lowercase(d):
 
     return dnew
 
+
 def parlist_to_list(l, property='value'):
     """
     Converts a list of Parameter class to a
@@ -106,6 +112,7 @@ def parlist_to_list(l, property='value'):
 
     return ol
 
+
 def sum_dict_keys(d):
     """
     Sums dictionary key records.
@@ -117,6 +124,7 @@ def sum_dict_keys(d):
         s += d[key]
     return s
 
+
 def read_text_file(f):
     """
     Reads an ASCII file and returns its string rep.
@@ -125,12 +133,13 @@ def read_text_file(f):
     Output:
       lines.. string representation of the file
     """
-    
+
     ifile = open(f, 'r')
     lines = ifile.readlines()
     ifile.close()
-    
+
     return lines
+
 
 def rotate_spectrum(wave, intens, vrot, fwhm=0.0, epsilon=0.6):
     """
@@ -142,51 +151,50 @@ def rotate_spectrum(wave, intens, vrot, fwhm=0.0, epsilon=0.6):
     output
       intens1.. rotated intensities
     """
-    
+
     if vrot > ZERO_TOLERANCE:
-	# we need it equidistant in RV
-	# lambda_n = lambda_0*(1+RV/c)**n
-	#n = len(wave)
-	#RV = ((wave[-1]/wave[0])**(1/(n-1))-1)/c.value
-	#indices = np.arange(n)
-	#wave_rv = wave[0]*(1-RV/c.value)**indices
-	wave_log = np.log(wave)
-	rv = np.linspace(wave_log[0], wave_log[-1], len(wave))
-	step = rv[1]-rv[0] 
-	
-	# interpolate
-	intens_rv = interpolate_spec(wave_log, intens, rv)
-	
-	# scale rotational velocity with light speed
-	vrot = 1000*vrot/c.value
-	
-	# get the kernel
-	#velocity vector
-	n = int(2*vrot/step)
-	rv_ker = np.arange(n)*step
-	rv_ker = rv_ker - rv_ker[-1]/2.
-	y = 1 - (rv_ker/vrot)**2
-	
-	# the kernel
-	kernel = (2*(1-epsilon)*np.sqrt(y) + np.pi*epsilon/2.*y)/(np.pi*vrot*(1-epsilon/3.0))
-        kernel = kernel/kernel.sum()
-        
+        # we need it equidistant in RV
+        # lambda_n = lambda_0*(1+RV/c)**n
+        # n = len(wave)
+        # RV = ((wave[-1]/wave[0])**(1/(n-1))-1)/c.value
+        # indices = np.arange(n)
+        # wave_rv = wave[0]*(1-RV/c.value)**indices
+        wave_log = np.log(wave)
+        rv = np.linspace(wave_log[0], wave_log[-1], len(wave))
+        step = rv[1] - rv[0]
+
+        # interpolate
+        intens_rv = interpolate_spec(wave_log, intens, rv)
+
+        # scale rotational velocity with light speed
+        vrot = 1000 * vrot / c.value
+
+        # get the kernel
+        # velocity vector
+        n = int(2 * vrot / step)
+        rv_ker = np.arange(n) * step
+        rv_ker = rv_ker - rv_ker[-1] / 2.
+        y = 1 - (rv_ker / vrot) ** 2
+
+        # the kernel
+        kernel = (2 * (1 - epsilon) * np.sqrt(y) + np.pi * epsilon / 2. * y) / (np.pi * vrot * (1 - epsilon / 3.0))
+        kernel = kernel / kernel.sum()
+
         # convolve the flux
-        intens_conv = fftconvolve(1-intens_rv, kernel, mode='same')
-        if n%2 == 1:
-	    rv = np.arange(len(intens_conv))*step+rv[0]
-	else:
-	    rv = np.arange(len(intens_conv))*step+rv[0]-step/2.
-	    
-	wave_conv = np.exp(rv)
-	
-	#interpolate back
-	intens = interpolate_spec(wave_conv, 1-intens_conv, wave)
-	
+        intens_conv = fftconvolve(1 - intens_rv, kernel, mode='same')
+        if n % 2 == 1:
+            rv = np.arange(len(intens_conv)) * step + rv[0]
+        else:
+            rv = np.arange(len(intens_conv)) * step + rv[0] - step / 2.
+
+        wave_conv = np.exp(rv)
+
+        # interpolate back
+        intens = interpolate_spec(wave_conv, 1 - intens_conv, wave)
+
     return intens
 
-        
-       
+
 def shift_spectrum(wave, RV):
     """
     input:
@@ -196,7 +204,6 @@ def shift_spectrum(wave, RV):
       new_wave.. shifted array of wavelengths
     """
     # shifts the wavelengths
-    new_wave = wave*(1+RV*1000/c.value)
-    
+    new_wave = wave * (1 + RV * 1000 / c.value)
+
     return new_wave
-    
