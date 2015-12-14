207ce46c66ac8c363f6d67e08543dea52b7883e9
chrysante87 <jana.nemravova@gmail.com>
2015-12-14 15:54:54 +0100

    Improved the plotting part of the hd81357 example.

--- pyterpol_examples/disentangled_spectra_fitting/hd81357/hd81357_plot.py
+++ pyterpol_examples/disentangled_spectra_fitting/hd81357/hd81357_plot.py
@@ -10,6 +10,19 @@ itf = pyterpol.Interface()
 # fill it with teh last session
 itf.load('hd81357.sav')
 
+# 2) Have a look at the comparisons
+itf.plot_all_comparisons(figname='finalfit')
+
+# 3) Export the disentangle spectra
+itf.write_synthetic_spectra(outputname='final_spectra')
+
+# 4) Have a look how everything converged
+itf.plot_convergence(figname='covergence_hd81357.png')
+
+# 5) Have look at uncertainty of the fit
+itf.plot_covariances(nbin=20)
+
+
 
 
 
