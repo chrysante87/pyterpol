a93d1aad39370760f0a428926353853f478d62fd
chrysante87 <jana.nemravova@gmail.com>
2015-12-14 10:21:52 +0100

    Added plotting of variance/covariance.

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import norm
 from pyterpol.synthetic.auxiliary import read_text_file
 
 def plot_convergence(block, labels=None, relative=True, savefig=True, figname=None):
@@ -74,6 +75,72 @@ def read_fitlog(f):
     fitlog['data'] = np.loadtxt(f)
 
     return fitlog
+
+
+def plot_chi2_map(x, y, nbin=10, labels=None, savefig=True, figname=None):
+    """
+    Plots a covariance map.
+    :param x parameter values
+    :param y parameter values
+    :param nbin number of bins in a histogram
+    :param labels
+    :param savefig
+    :param figname
+    :return:
+    """
+    # if user did not pass the labels
+    if labels == None:
+        labels = ['x', 'y']
+
+    # set up the figure
+    fig = plt.figure(figsize=(10,10), dpi=100)
+    var_axes = [(2, 2, 1), (2, 2, 4)]
+    var_data = [x, y]
+
+    # firs the plot of the variance
+    for i in range(0, 2):
+        ax = fig.add_subplot(var_axes[i], aspect=1.0)
+
+        # plot the histogram
+        n, bins, patches = ax.hist(var_data[i], normed=True, label=labels[0])
+        x_g = np.linspace(bins.min(), bins.max(), 50)
+
+        # plot the gaussian 'fit'
+        mean = var_data[i].mean()
+        var = var_data[i].std(ddof=1)
+        g = norm(loc=mean, scale=var)
+        ax.plot(x_g, g.pdf(x_g), 'r-')
+
+        # labeling
+        ax.set_xlabel(labels[0])
+        ax.set_ylabel('$n_i/N$')
+        ax.title('$\sigma_%s$ = %.3f' % (labels[0], var))
+
+    # plot the 2d chi2 map
+    ax = fig.add_subplot(223, aspect=1.0)
+    ax.hist2d(x, y, nbin, normed=True)
+
+    # compute the correlation
+    cov = ((x-x.mean())*(y-y.mean())).mean()
+    cor = cov/(x.std(ddof=1)*y.std(ddof=1))
+
+    # labelling
+    ax.set_xlabel(labels[0])
+    ax.set_ylabel(labels[1])
+    ax.set_title(r'$\rho(%s, %s) = %.3f$' % (labels[0], labels[1], cor))
+
+    # save the figure
+    if savefig:
+        if figname is None:
+            figname = 'covariance' + '_'.join(labels) + 'png'
+
+        plt.savefig(figname)
+
+
+
+
+
+
 
 
 
