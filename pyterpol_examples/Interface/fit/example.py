890c7fa811b3de9f294e9180f2136689ae92605d
chrysante87 <jana.nemravova@gmail.com>
2015-12-16 16:53:11 +0100

    Working on the example of the fitting.

--- pyterpol_examples/Interface/fit/example.py
+++ pyterpol_examples/Interface/fit/example.py
@@ -20,7 +20,62 @@ import pyterpol
 # session.
 itf = pyterpol.Interface.load('setup.itf')
 
-# review the loaded interface
+# review the loaded interface - if we compare it with the
+# previous example, we see that everything loaded as it
+# should
 print itf
+
+"""
+==============================================StarList==============================================
+Component: primary
+name: rv value: 0.0 vmin: -1000.0 vmax: 1000.0 fitted: False group: 1 _typedef: None
+name: rv value: 0.0 vmin: -1000.0 vmax: 1000.0 fitted: False group: 2 _typedef: None
+name: rv value: 0.0 vmin: -1000.0 vmax: 1000.0 fitted: False group: 3 _typedef: None
+name: teff value: 25000.0 vmin: 6000.0 vmax: 50000.0 fitted: False group: 0 _typedef: None
+name: vrot value: 150.0 vmin: 0.0 vmax: 500.0 fitted: False group: 0 _typedef: None
+name: logg value: 4.2 vmin: 0.0 vmax: 5.0 fitted: False group: 0 _typedef: None
+name: lr value: 0.7 vmin: 0.0 vmax: 1.0 fitted: False group: 0 _typedef: None
+name: z value: 1.0 vmin: 0.0 vmax: 2.0 fitted: False group: 0 _typedef: None
+Component: secondary
+name: rv value: 0.0 vmin: -1000.0 vmax: 1000.0 fitted: False group: 1 _typedef: None
+name: rv value: 0.0 vmin: -1000.0 vmax: 1000.0 fitted: False group: 2 _typedef: None
+name: rv value: 0.0 vmin: -1000.0 vmax: 1000.0 fitted: False group: 3 _typedef: None
+name: teff value: 18000.0 vmin: 6000.0 vmax: 50000.0 fitted: False group: 0 _typedef: None
+name: vrot value: 50.0 vmin: 0.0 vmax: 500.0 fitted: False group: 0 _typedef: None
+name: logg value: 4.2 vmin: 0.0 vmax: 5.0 fitted: False group: 0 _typedef: None
+name: lr value: 0.3 vmin: 0.0 vmax: 1.0 fitted: False group: 0 _typedef: None
+name: z value: 1.0 vmin: 0.0 vmax: 2.0 fitted: False group: 0 _typedef: None
+=============================================RegionList=============================================
+Region name: region00: (wmin, wmax) = (6330.0, 6375.0):
+component: all groups: {'lr': 0}
+Region name: region01: (wmin, wmax) = (6500.0, 6600.0):
+component: all groups: {'lr': 0}
+============================================ObservedList============================================
+List of all attached spectra:
+filename: a component: all korel: False loaded: True hasErrors: True global_error: 0.001 group: {'rv': 1} (min, max): (6250.0, 6799.9499999999998)
+filename: b component: all korel: False loaded: True hasErrors: True global_error: 0.001 group: {'rv': 2} (min, max): (6250.0, 6799.9499999999998)
+filename: c component: all korel: False loaded: True hasErrors: True global_error: 0.001 group: {'rv': 3} (min, max): (6250.0, 6799.9499999999998)
+===============================================Fitter===============================================
+Fitter: None optional_arguments: {}
+Initial parameters:
+====================================================================================================
+"""
+
+# the second step is to set what will be fitted. Do not forget to
+# set the boundaries vmin, vmax too.
+
+# we can do it parameter by parameter
+itf.set_parameter(group=1, component='primary', parname='rv', fitted=True)
+
+# or we can set all at once
+itf.set_parameter(group=1, component='primary', parname='rv', fitted=True, vmin=-120., vmax=120.)
+
+# or set everything for primary
+itf.set_parameter(component='primary', parname='rv', fitted=True, vmin=-120., vmax=120.)
+
+# or set everything for every rv in the StarList
+itf.set_parameter(parname='rv', fitted=True, vmin=-120., vmax=120.)
+
+
 
 
